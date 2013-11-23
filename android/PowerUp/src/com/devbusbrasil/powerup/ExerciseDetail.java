e2506caf34e355b2c5367eda99c7725dbf0b2e1a
Bruno Cicanci <brunocicanci@gmail.com>
2013-11-23 12:38:34 -0200

    Adding android project

---
+++ android/PowerUp/src/com/devbusbrasil/powerup/ExerciseDetail.java
@@ -0,0 +1,31 @@
+package com.devbusbrasil.powerup;
+
+import com.devbusbrasil.powerup.lib.QRCode;
+import com.devbusbrasil.powerup.lib.TrainingItem;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ExerciseDetail extends Activity {
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        
+        TrainingItem trainingItem = QRCode.GetInstance().getTrainingItem();
+        QRCode.GetInstance().setCode(null);
+        
+        TextView tv = (TextView)findViewById(R.id.textView1);
+        
+        if (trainingItem != null) {
+            tv.setText(trainingItem.getText());
+        }
+        else {
+            
+            tv.setText("N‹o encontrado");
+        }
+    }
+
+}
---
+++ android/PowerUp/src/com/devbusbrasil/powerup/UserDetail.java
@@ -0,0 +1,7 @@
+package com.devbusbrasil.powerup;
+
+import android.app.Activity;
+
+public class UserDetail extends Activity {
+
+}
